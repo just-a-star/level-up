c5b9d43db2d23be25accce595c6d0e436ba062cb
Bintang Rahmatullah <rahmatbintang181@gmail.com>
2024-06-10 14:16:38 +0700

    feat: 🎸 added authentication sign in & sign up, db init

---
+++ src/lib/db.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export const db = prisma;
